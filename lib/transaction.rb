e494453e4fd6b8ab1863f3494a9f1032be371c52
Joshua Retallick <josh@old-empire.co.uk>
2021-01-11 15:58:23 +0000

    Stubbed timestamp to test it was working

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,8 +3,13 @@ class Transaction
   attr_reader :amount, :timestamp
 
   def initialize
-    @amount = amount
+    @amount = amount.to_f
     @timestamp = Time.now.getutc
   end
+
+  def to_s
+    type = @amount > 0 ? "credit" : "debit"
+    "#{@timestamp} - #{type} - #{@amount}"
+  end 
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -10,4 +10,11 @@ describe Transaction do
     expect(subject.timestamp).to be_an_instance_of Time
   end
 
+  describe "#to_s method" do
+    it "Displays transaction in a readable format" do
+      Time.stub(:now).and_return(Time.mktime(1970,1,1))
+      expect(subject.to_s).to eq "1969-12-31 23:00:00 UTC - debit - 0.0"
+    end
+  end
+
 end
